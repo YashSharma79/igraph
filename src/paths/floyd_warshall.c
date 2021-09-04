ca6f35d331444e4ec93a083afb6e61bf992a2f66
Yash Sharma <yashsharma2595@gmail.com>
2021-09-04 14:53:25 -0400

    Used igraph data structures instead of pure C

--- src/paths/floyd_warshall.c
+++ src/paths/floyd_warshall.c
@@ -1,62 +1,73 @@
-'#'define INF 9999999
 
 using namespace std;
 
-int main()
+int igraph_shortest_paths_floyd_warshall(const igraph_t *graph,
+                                       igraph_matrix_t *res,
+                                       const igraph_vs_t from,
+                                       const igraph_vs_t to,
+                                       const igraph_vector_t *weights,
+                                       igraph_neimode_t mode)
 {
-    int n,m,i,x,y,r,a,b,j,k,q;
-    cin>>n>>m;
-    int adj[n][n];
 
-    for(i=0;i<n;i++)
+    // replace n with number_of_nodes
+    long int no_of_nodes = igraph_vcount(graph);
+    igraph_real_t my_infinity = IGRAPH_INFINITY;
+
+    long int i, j, k;
+
+    igraph_matrix_t adj;
+    igraph_matrix_init(&adj,no_of_nodes, no_of_nodes);
+
+    // array of minimum distances initialized to ∞ (infinity)
+    for (i = 0; i < no_of_nodes; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < no_of_nodes; j++)
         {
-            adj[i][j]=INF;
+            igraph_matrix_t[i][j] = my_infinity;
         }
     }
 
-    for(i=0;i<n;i++) {
-        adj[i][i]=0;
+    // distance to the same node = 0
+    for (i = 0; i < no_of_nodes; i++)
+    {
+        igraph_matrix_t[i][i] = 0;
     }
 
-    int floyd[n][n];
+    igraph_matrix_t floyd;
+    igraph_matrix_init(&floyd, no_of_nodes, no_of_nodes);
 
-    for(i=0;i<m;i++)
+    // assign weight of i, j as shortest distance
+    for (i = 0; i < no_of_nodes; i++)
     {
-        cin>>x>>y>>r;
-        adj[x-1][y-1]=r;
-    }
-
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
+        for (j = 0; j < no_of_nodes; j++)
         {
-            floyd[i][j]=adj[i][j];
+            MATRIX(*floyd, i, j) = MATRIX(*adj, i, j)
         }
     }
 
-    for(k=0;k<n;k++)
+    for (k = 0; k < no_of_nodes; k++)
     {
-        for(i=0;i<n;i++)
+        for (i = 0; i < no_of_nodes; i++)
         {
-            for(j=0;j<n;j++)
+            for (j = 0; j < no_of_nodes; j++)
             {
-                floyd[i][j]=min(floyd[i][j],floyd[i][k]+floyd[k][j]);
+                MATRIX(*floyd, i, j) = min(MATRIX(*floyd, i, j), MATRIX(*floyd, i, k) + MATRIX(*floyd, k, j));
             }
         }
     }
 
-    cin>>q;
 
-    while(q--)
+    /* For testing only
+    cin >> q;
+    while (q--)
     {
-        cin>>a>>b;
-        int ans=floyd[a-1][b-1];
-        if(ans!=INF)
-            cout<<ans<<endl;
+        cin >> a >> b;
+        int ans = floyd[a - 1][b - 1];
+        if (ans != INF)
+            cout << ans << endl;
         else
-            cout<<"-1\n";
+            cout << "-1\n";
     }
+    */
     return 0;
 }
